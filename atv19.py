1e29508cfbee8409c1011c5205f7050d4a706689
Leison44 <Lps44de@gmail.com>
2025-12-20 18:23:01 -0300

    Delete atv19.py

--- atv19.py
+++
@@ -1,8 +0,0 @@
-A = int(input("Digite o valor de A: "))
-B = int(input("Digite o valor de B: "))
-i = A
-print(f"Números ímpares entre {A} e {B}:")
-while i <= B:
-    if i % 2 != 0:  
-        print(i, end=" ")
-    i += 1
