a2965e9b01100655f8be455fd621078a9139822e
Leison44 <Lps44de@gmail.com>
2025-12-20 17:56:48 -0300

    Delete atv15.py

--- atv15.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-print("Digite números positivos (digite um número negativo para parar):")
-while True:
-    numero = int(input("Digite um número: "))
-    if numero < 0:
-        break 
-    soma += numero  
-print(f"A soma dos números positivos digitados é {soma}")
